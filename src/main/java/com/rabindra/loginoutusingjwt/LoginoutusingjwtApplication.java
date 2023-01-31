f8d9229e948145eca7ed7feac78a2a5d9afec844
rabindra <rabindra@red27mobile.com>
2023-01-31 13:30:22 +0545

    lgin/lout initial

---
+++ src/main/java/com/rabindra/loginoutusingjwt/LoginoutusingjwtApplication.java
@@ -0,0 +1,13 @@
+package com.rabindra.loginoutusingjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginoutusingjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginoutusingjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabindra/loginoutusingjwt/LoginoutusingjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabindra.loginoutusingjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginoutusingjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
